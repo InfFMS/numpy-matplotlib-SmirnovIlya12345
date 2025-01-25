96d3899e5db854fc269c812d503b61aeb5ded44d
SmirnovIlya12345 <betelgeuse08si@yandex.ru>
2025-01-25 11:47:57 +0700

    Update wether.py

--- wether.py
+++ wether.py
@@ -13,12 +13,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 x=np.linspace(1,365,365)
 y=[]
+hot=cold=maxcold=y_10=y_5=y0=y5=y10=y15=y20=y25=y30=0
 for i in range(365):
-    y.append(randint(-10,25))
-fig, axs = plt.subplots(1, 2, figsize=(10, 4))
-axs[0].plot(x, y)
-axs[0].set_xlabel("Days")
-axs[0].set_ylabel("Temperature")
+    y2=randint(-10,35)
+    y.append(y2)
+    if y2>25:
+        hot+=1
+for i in range(len(y)):
+    if y[i]>=30:
+        y30+=1
+    elif y[i]>=25:
+        y25+=1
+    elif y[i]>=20:
+        y20+=1
+    elif y[i]>=15:
+        y15+=1
+    elif y[i]>=10:
+        y10+=1
+    elif y[i]>=5:
+        y5+=1
+    elif y[i]>=0:
+        y0+=1
+    elif y[i]>=-5:
+        y_5+=1
+    elif y[i]>=-10:
+        y_10+=1
+    if y[i]<0:
+        cold+=1
+        if cold>maxcold:
+            maxcold=cold
+    else:
+        cold=0
+print('There was',hot,'hot days')
+print('Longest cold days break:',maxcold)
+plt.plot(x,y)
 plt.show()
-plt.bar(x, y, color='blue')
+temp=['-10','-5','0','5','10','15','20','25','30']
+yes=[y_10,y_5,y0,y5,y10,y15,y20,y25,y30]
+plt.bar(temp,yes,color='red')
 plt.show()
