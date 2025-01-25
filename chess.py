56b4a78a0cb94d1ea395a137255121ae4d15fe93
SmirnovIlya12345 <betelgeuse08si@yandex.ru>
2025-01-25 11:48:30 +0700

    Update chess.py

--- chess.py
+++ chess.py
@@ -10,17 +10,17 @@ row=int(input())
 fig, ax = plt.subplots()
 data = np.array(([0,10,0,10,0,10,0,10],[10,0,10,0,10,0,10,0],[0,10,0,10,0,10,0,10],[10,0,10,0,10,0,10,0],[0,10,0,10,0,10,0,10],[10,0,10,0,10,0,10,0],[0,10,0,10,0,10,0,10],[10,0,10,0,10,0,10,0]))
 queen=[row,file]
-circle = plt.Circle((file, row), 0.5, color= "red", label="Окружность")
+circle = plt.Circle((file, row), 0.5, color= "white", label="Окружность")
 ax.add_patch(circle)
-data[:,file]=data[:,file]+2
-data[row,:]=data[row,:]+2
+data[:,file]=data[:,file]=4
+data[row,:]=data[row,:]=4
 for i in range(row-8, 8-row):
     if file+i<=7 and file+i>=0 and row+i<=7 and row+i>=0:
-        data[row+i,file+i]=data[row+i,file+i]+2
+        data[row+i,file+i]=data[row+i,file+i]=4
 for i in range(file-100, 100-file):
     if file-i<=7 and file-i>=0 and row+i<=7 and row+i>=0:
-        data[row+i,file-i]=data[row+i,file-i]+2
-data[row,file]=data[row,file]-8
+        data[row+i,file-i]=data[row+i,file-i]=4
+data[row,file]=data[row,file]
 plt.imshow(data, cmap='hot')
 plt.colorbar(label="")
 plt.title("Шахматная доска")
